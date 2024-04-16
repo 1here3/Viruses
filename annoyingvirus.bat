d145307705d4e912e33b238df2952b0c86178cbf
1here3 <159972256+1here3@users.noreply.github.com>
2024-04-15 17:29:58 -0700

    Add files via upload

---
+++ annoyingvirus.bat
@@ -0,0 +1,51 @@
+ @echo off
+1:
+color a
+echo winininininit
+set /p input=
+if /i %input%==Ygr goto wininit
+if /i %input%==wininit goto crash
+if /i not %input%== Ygr,wininit goto 1
+:Ygr
+echo            OOOOOOO             OOOOOOO
+                 OOOOOOO           OOOOOO
+                  OO0O0OO         OOOOOO
+                   OOOOOOOO     0000000
+                        0000000000
+                          000000
+                            00
+
+
+
+                   00000         00000
+                    00000       00000
+                     00000     00000
+                      00000   00000 
+                       0000000000
+                      00000   00000
+                     00000     00000
+                    00000       00000
+                   00000         00000
+             wasd                      dsaw
+
+
+
+m                        8889
+                      0827487489
+              734378463874637846346938462
+273827436473648736478364213467463764736473467434646746476
+i4384934893482984924883748euhey37437ry3746374y7ey74ye73ye7y
+34983e94u9eu8euu83ue8u3e8u38ue83u8ue83u8ueeui3ejiwjswwsmam
+wedfghjkjhgfghjkjhgfghjkjhwegfhjkdlskdjfhjdkslkdjfhjdksdjf
+\hgfjdkasjisdjwierh4ucyhru4ruheurhurhufhhbvhbvbdv dbvhvbams
+jbdwhdwsbxsbdusbxsbhdxbssnxbznxbznxbznxbznxbznxbznxbznxbnxb
+znxbznxbznxbznxbznxbznxbhjdhuwieyhduehuhdeudhuehdehkdsdsmd
+kdsdjskdjskdjdksdkskdkskdjksdjksdjksjdksjdksdjksjdkjsdksjdk
+ awexssasassasasasasasaasasassassasasasasasasasasasassasdd
+timeout 50
+pause
+exit
+:crash
+echo fbrandybrandyk your PC
+timeout 10
+shutdown /r /t 666
